4bd8a212eea2a2d74b64c6ecd2f319a003d224c9
emabarra <em.barra@duocuc.cl>
2026-05-21 18:52:54 -0400

    se agrega Dockerfil, docker-compose a repositorio

--- src/test/java/com/devops/springboot_app_devops/SpringbootAppDevopsApplicationTests.java
+++ src/test/java/com/devops/springboot_app_devops/SpringbootAppDevopsApplicationTests.java
@@ -3,11 +3,18 @@ package com.devops.springboot_app_devops;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+    "spring.datasource.url=jdbc:h2:mem:usuariosdb;DB_CLOSE_DELAY=-1;MODE=MySQL",
+    "spring.datasource.driver-class-name=org.h2.Driver",
+    "spring.datasource.username=sa",
+    "spring.datasource.password=",
+    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+    "spring.jpa.hibernate.ddl-auto=create-drop"
+})
 class SpringbootAppDevopsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        // Test básico de carga de contexto superado usando H2 en memoria
+    }
 }
